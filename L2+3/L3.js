7014b452e3cf1ad3e2e36cd4c437ab36d1a66ea6
Neptune-enCD <neptune.en9525@gmail.com>
2026-04-27 12:07:42 +0100

    Update L3.js

--- L2+3/L3.js
+++ L2+3/L3.js
@@ -33,6 +33,8 @@ function write (value,title,priority,overwrite) {
  * @param {string} title - the unique name to search for. leave '' if you want to search with ID instead
  * @param {number} id - (NOT RECOMMENDED) the id to search for. ignore if you want to search for title
  * 
+ * if title is not found in the SSD, null will be outputted
+ *
  * ONLY ONE. DO NOT ENTER BOTH OR NEITHER!
  */
 function read (title,id) {
