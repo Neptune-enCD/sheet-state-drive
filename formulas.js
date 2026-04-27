a7772078f7a57488dabf14c7ff63df3d2f724b41
Neptune-enCD <neptune.en9525@gmail.com>
2026-04-27 11:24:31 +0100

    Update formulas.js
    fixed the non-priority filter

--- formulas.js
+++ formulas.js
@@ -15,4 +15,14 @@
 ))), 0)
 
 //paste into cell C1
-=MIN(FILTER(SEQUENCE(1900, 1, 101), ARRAYFORMULA(ISBLANK(MAP(SEQUENCE(1900, 1, 101), LAMBDA(id, INDIRECT(ADDRESS( (FLOOR((SQRT(8*id-7)-1)/2) - (id - (FLOOR((SQRT(8*id-7)-1)/2)*(FLOOR((SQRT(8*id-7)-1)/2)+1)/2)) + 1)*2, id - (FLOOR((SQRT(8*id-7)-1)/2)*(FLOOR((SQRT(8*id-7)-1)/2)+1)/2) ))))))))
+=MATCH(TRUE, MAP(SEQUENCE(10000, 1, 101), LAMBDA(n, LET(
+  w, CEILING((SQRT(8*n+1)-1)/2)-1,
+  t, (w*w+w)/2,
+  y, n-t,
+  x, w-y+2,
+  r_raw, IF(MOD(w,2)=0, y, x),
+  c_raw, IF(MOD(w,2)=0, x, y),
+  r, MAX(1, r_raw),
+  c, MAX(1, c_raw),
+  ISBLANK(OFFSET(A2, (r*2)-2, c-1))
+))), 0) + 100
